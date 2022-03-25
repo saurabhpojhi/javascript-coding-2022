687e9f47c7fa72974c0c5f221c686ad253ee9335
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-26 00:41:47 +0530

    add

---
+++ Variables.js
@@ -0,0 +1,11 @@
+// 4 Ways to Declare a JavaScript Variable:
+// Using var
+// Using let
+// Using const
+
+//In this example, x, y, and z, are variables, declared with the var keyword:
+
+var x= 4;
+var y =5;
+var z= x+y;
+console.log(z);
