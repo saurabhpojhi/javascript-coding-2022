e363f75d72c80fe388d134e66ec63bdc679f0c98
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-27 22:34:46 +0530

    Hoisting

---
+++ HoistingJavaScript.js
@@ -0,0 +1,14 @@
+
+// console.log(name);   // uncought Refrence error
+// let name ="Saurabh";
+
+// note variable decleared with let and const.
+
+//ReferenceError: Cannot access 'name' before initialization
+
+//  VAriable Hoisting with Var      //
+
+// console.log(name); //  this will show  undefined 
+//  var name = "saurabh";
+// console.log(name);
+
