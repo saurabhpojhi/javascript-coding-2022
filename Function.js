1fcff978a7b155ba5dfffd6c4bcd18c830c11ff7
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-27 22:45:41 +0530

    function

---
+++ Function.js
@@ -0,0 +1,14 @@
+function name(parameter1, parameter2, parameter3) {
+    // code to be executed
+}
+
+// Calculate the product of two numbers, and return the result:
+
+var  x = myFunction(4, 3);
+
+function myFunction(a, b) {
+    return a * b;
+  
+}
+
+// 
