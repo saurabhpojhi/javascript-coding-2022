3aee8ea0b71b872db7695456828698553c332fed
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-27 22:59:09 +0530

    Object

--- Object.js
+++ Object.js
@@ -1,1 +1,18 @@
 // Objects and methods in JavaScript
+
+// How to create objects in javascript;
+
+const person ={
+  name:"saurabh"
+};    // this is simple and most popular way 
+
+// you can also use  New keyword
+
+const person = new Object();
+person.name='saurabh';
+
+ // you can also use  'New'  with user defined constructor function 
+
+ function person (name){
+     this.name=name;
+ }
