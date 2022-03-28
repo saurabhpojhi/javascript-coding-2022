5ce821694d2c96bf02fa20f1db9a420c48081a23
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-28 23:35:25 +0530

    String

--- String.js
+++ String.js
@@ -1,6 +1,15 @@
  // javaScript String Method
 
- // #1 Length
+ // #1 Length    // check the length of a string
 
  const myString = "I love coding"
  console.log(myString.length);
+
+ // #2  Trim     // Remove whiteSpaces at the beginnig and at the end
+
+ const myString1 = "  I love coding  "
+ console.log(myString1.length)
+// 17
+console.log(myString1.trim());
+// I love coding
+console.log(myString1.trim().length);
