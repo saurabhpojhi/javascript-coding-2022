266a9504c67f0dd94efbc67204fd0d56437818eb
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-28 23:40:53 +0530

    String

--- String.js
+++ String.js
@@ -13,3 +13,17 @@
 console.log(myString1.trim());
 // I love coding
 console.log(myString1.trim().length);
+
+// # Split   // convert a string to an array
+
+const myString2 = "I love coding";
+const arr = myString2.split("");
+console.log(arr);
+// [
+//     'I', ' ', 'l', 'o',
+//     'v', 'e', ' ', 'c',
+//     'o', 'd', 'i', 'n',
+//     'g'
+//   ]
+
+// #
