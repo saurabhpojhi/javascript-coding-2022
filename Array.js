f97f64f8782c15391853c3976b70cbe8acbf7e0b
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-30 23:45:09 +0530

    Array

--- Array.js
+++ Array.js
@@ -6,3 +6,16 @@
    let joinedArray = primeNumber.concat(evenNumber);
    console.log(joinedArray);
    
+   // #2 Using concat() method                    //
+
+   var languages1 = ["javascript", "python","java"];
+   var languages2 = ["c","C++"];
+
+   var newArr1 = languages1.concat(languages2);     // concatenating two arrays
+
+   console.log(newArr1);
+
+   var newArr2 =languages1.concat("Erlang", languages2);   // concatenating a value and array
+   console.log(newArr2);
+
+   
