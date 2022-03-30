48e091ff790dc6ff75c102a9c12b8dabe6db0265
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-03-30 23:38:37 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,8 @@
+   // #1 JavaScript Array concat()  // JavaScript Array concat()
+
+   let primeNumber = [2,3,5,7]
+   let evenNumber = [2,4,6,8]
+
+   let joinedArray = primeNumber.concat(evenNumber);
+   console.log(joinedArray);
+   
