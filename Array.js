ea0f3db7a061a3ad7d7c73bf0ffc5db39ec16d1b
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-04-03 23:56:53 +0530

    Array

--- Array.js
+++ Array.js
@@ -73,6 +73,7 @@ console.log(programmingLanguges2);
 
 // #6 Javascript Array filter()     // The filter() method returns a new array with all elements that pass the test defined by the given function.
 
+// 1: Filtering out values from Array
 
 
 // #7 JavaScript Array findIndex()
