4d88d3be2d40ac1876db6c947ef2bda1e710815a
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-04-17 00:31:12 +0530

    Array Handbook

--- ArrayHandbook.js
+++ ArrayHandbook.js
@@ -1,1 +1,8 @@
 /* What is an Array in JavaScript */
+
+// A pair of square brackets [] represents an array in JavaScript. All the elements in the array are comma(,) separated.
+// you can create an array with elements of type String, Boolean, Number, Objects, and even other Arrays.
+
+// example of an array with four elements: type Number, Boolean, String, and Object.
+const mixedTypeArray=[100, true, 'github', {}] ;
+
