fb4b8600731a894b649667537ed428ed26b9d47a
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-04-16 23:30:33 +0530

    varLetConst

--- varLetConst.js
+++ varLetConst.js
@@ -19,27 +19,27 @@ const javascript = "varLetString";
 
 /* If you do not want a variable declared inside a { } block to be accessed outside of the block, you need to declare them using the let or const keywords. Variables declared with the var keyword inside the { } block are accessible outside of the block too. So, be careful.*/
 
-{
-    let first_name ="saurabh";
-    const pincode =841219;
-    var age = 26;
+// {
+//     let first_name ="saurabh";
+//     const pincode =841219;
+//     var age = 26;
 
-}
-console.log(first_name);  // ReferenceError: first_name is not defined
-console.log(pincode);     // ReferenceError: pincode is not defined
-console.log(age);      // 26
+// }
+// console.log(first_name);  // ReferenceError: first_name is not defined
+// console.log(pincode);     // ReferenceError: pincode is not defined
+// console.log(age);      // 26
 // Do not use the var keyword inside a block (block scope). Always use let and const instead.
 
 /*How to Use JavaScript Variables in Functional Scope*/
-function f1(){
-    let first_name ="saurabh";
-    const pincode =841219;
-    var age = 26;
-}
-f1();
-console.log(first_name);  // ReferenceError: first_name is not defined
-console.log(pincode);     // ReferenceError: pincode is not defined
-console.log(age);      // ReferenceError: age is not defined
+// function f1(){
+//     let first_name ="saurabh";
+//     const pincode =841219;
+//     var age = 26;
+// }
+// f1();
+// console.log(first_name);  // ReferenceError: first_name is not defined
+// console.log(pincode);     // ReferenceError: pincode is not defined
+// console.log(age);      // ReferenceError: age is not defined
 // none of the variables are accessible outside of the function, not even age which is declared using var. So, the conclusion is,
 //The variable declared with var inside a function is not accessible outside of it. The keyword var has function-scope.
 
@@ -47,3 +47,19 @@ console.log(age);      // ReferenceError: age is not defined
 
 // Variables declared outside of any functions and blocks are global and are said to have Global Scope. This means you can access them from any part of the current JavaScript program.
 // You can use var, let, and const to declare global variables. 
+
+let first_name1 ="saurabh";
+const mobile= 7277478119;
+var age = 27;
+
+function f2(){
+    console.log(first_name1);  // saurabh
+    console.log(mobile);     // 7277478119
+    console.log(age);  // 27
+}
+f2();
+
+  // or if you want to below code that you can write you will get same output
+   console.log(first_name1);  // saurabh
+    console.log(mobile);     // 7277478119
+    console.log(age);  // 27
