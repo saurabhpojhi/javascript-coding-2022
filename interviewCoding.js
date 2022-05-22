23df341b0bcc2e9f8e01dff7fbdf9f0875b578aa
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-23 00:41:12 +0530

    convert object {} to array []

--- interviewCoding.js
+++ interviewCoding.js
@@ -21,3 +21,26 @@ console.log(myObj1);
 let arrayList2 = ['Tommy','Jerry','Micky','Mouse'];
 let myObj2 = {...arrayList2}
 console.log(myObj2);
+
+// 2. convert object {} to array []  in javascript 
+
+// method 1 for loop
+
+let objList = {'a':1,'b':2,'c':3,'d':4,'e':5};
+let arrList = [];
+for(key in objList){
+    arrList.push([key,objList[key]])
+}
+console.log(arrList);  //[ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ], [ 'd', 4 ], [ 'e', 5 ] ]
+
+// method 2 object.key or map 
+let objList1 = {'a':1,'b':2,'c':3,'d':4,'e':5};
+console.log(Object.keys(objList1));  // it return all the object key   like [ 'a', 'b', 'c', 'd', 'e' ]
+
+//  map 
+let objList2 = {'a':1,'b':2,'c':3,'d':4,'e':5};
+let arrList1 = Object.keys(objList2).map(key =>{
+    return [key, objList2[key]];
+});
+
+console.log(arrList1);  // [ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ], [ 'd', 4 ], [ 'e', 5 ] ]
